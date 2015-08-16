ab9180f7229c4234a3d674c82d350ef397599664
mlazovla <mlazovla@gmail.com>
2015-08-17 00:10:19 +0200

    [add] ticket and reg forms

--- app/FrontModule/presenters/EventPresenter.php
+++ app/FrontModule/presenters/EventPresenter.php
@@ -27,6 +27,7 @@ class EventPresenter extends BasePresenter
 			$this->error('Akci nelze otevřít', 404);
 		}
 		$this->template->event = $event;
+		$this->template->now = new \DateTime('now');
 	}
 
 	public function renderDefault() {
@@ -36,6 +37,78 @@ class EventPresenter extends BasePresenter
 		$this->template->eventMain = $eventMain->where('main = ?', 1)->where('date > ?', new \DateTime('now'));
 		$this->template->eventNotMain = $eventNotMain->where('main != ?', 1)->where('date > ?', new \DateTime('now'));
 		$this->template->eventPast = $eventPast->where('date < ?', new \DateTime('now'))->limit(15);
-
 	}
+
+	protected function createComponentEventRegistrationForm()
+	{
+		$form = new Nette\Application\UI\Form();
+
+		$form->addText('name', 'Jméno')
+			->setRequired('Vyplňte prosím jméno')
+			->setAttribute('class', 'input-sm');
+
+		$form->addText('surname', 'Příjmení')
+			->setRequired('Vyplňte prosím příjmení')
+			->setAttribute('class', 'input-sm');
+
+		$form->addText('birthdate', 'Datum narození')
+			->setType('date')
+			->setRequired('Vyplňte prosím datum narození')
+			->setAttribute('class', 'input-sm');
+
+		$form->addText('phone',  'Mobilní telefon')
+			->setType('tel')
+			->setAttribute('class', 'input-sm');
+
+		$form->addText('email', 'Email')
+			->setType('email')
+			->addRule($form::EMAIL, 'Zkontrolujte si tvar emailové adresy')
+			->setRequired('Vyplňte prosím email')
+			->setAttribute('class', 'input-sm');
+
+		$form->addTextArea('note', 'Poznámka')
+			->setAttribute('class', 'input-sm');
+
+		$form->addHidden('event_id');
+
+		$form->addSubmit('send', 'Registrovat')
+			->setAttribute('class', 'btn btn-primary');
+
+		$form->onSuccess[] = array($this, 'eventRegistrationFormSucceeded');
+
+		return $form;
+	}
+
+	public function eventRegistrationFormSucceeded(Nette\Application\UI\Form $form, $values)
+	{
+		$this->flashMessage('Byl jste registrován.');
+		$this->redirect('this');
+	}
+
+	protected function createComponentEventTicketForm()
+	{
+		$form = new Nette\Application\UI\Form();
+
+		$form->addText('email', 'Email')
+			->setType('email')
+			->addRule($form::EMAIL, 'Zkontrolujte si tvar emailové adresy')
+			->setRequired('Vyplňte prosím email')
+			->setAttribute('class', 'input-sm');
+
+		$form->addHidden('event_id');
+
+		$form->addSubmit('send', 'Získat lístek')
+			->setAttribute('class', 'btn btn-primary');
+
+		$form->onSuccess[] = array($this, 'eventTicketFormSucceeded');
+
+		return $form;
+	}
+
+	public function eventTicketFormSucceeded(Nette\Application\UI\Form $form, $values)
+	{
+		$this->flashMessage('Byla Vám zaslána vstupenka.', 'success');
+		$this->redirect('this');
+	}
+
 }
